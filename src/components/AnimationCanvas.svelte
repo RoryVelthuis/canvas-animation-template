fcdea418c059dc8dc3f144b1ce842a07dd5a51c4
RoryVelthuis <RorySVelthuis@gmail.com>
2024-09-24 05:36:36 +1200

    created canvasHelpers.js to hold functions related to canvas management

--- src/components/AnimationCanvas.svelte
+++ src/components/AnimationCanvas.svelte
@@ -1,17 +1,14 @@
 <script>
     import { onMount } from 'svelte';
+    import { initCanvas, resizeCanvas } from '../lib/canvasHelpers';
     
     let canvas;
-    let animationFrameId;
     let ctx;
+    let animationFrameId;
 
-    let mousePosition = { x: 0, y: 0 };
-
-    //Inialize canvas
-    function initCanvas() {
+    function initalize() {
+        // Get canvas context
         ctx = canvas.getContext('2d');
-        canvas.width = canvas.clientWidth;
-        canvas.height = canvas.clientHeight;
     }
 
     // Animation loop
@@ -19,7 +16,6 @@
         // Update and render
         update();
         render();
-
         // Request next frame
         animationFrameId = requestAnimationFrame(animate);
     }
@@ -33,32 +29,20 @@
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         // Draw something
-    }
 
-    // Event listeners
-    function resizeCanvas() {
-        canvas.width = canvas.clientWidth;
-        canvas.height = canvas.clientHeight;
-    }
-
-    function handleMouseMove(event) {
-        const rect = canvas.getBoundingClientRect();
-        mousePosition = {
-            x: event.clientX - rect.left,
-            y: event.clientY - rect.top
-        };
-        // console.log(`Mouse Position: ${mousePosition.x}, ${mousePosition.y}`);
     }
 
 
     onMount(() => {
-        console.log('Animation Canvas mounted');
-        initCanvas();
-        animate();
+        // Initialize the canvas
+        initalize();
 
         // Handle window resize event
         window.addEventListener('resize', resizeCanvas);
         console.log('Resize event listener added');
+
+         // Start the animation loop
+        animate();
 
         // Cleanup on component destroy
         return () => {
@@ -70,7 +54,7 @@
     })
 </script>
 
-<canvas bind:this={canvas} on:mousemove={handleMouseMove} ></canvas>
+<canvas bind:this={canvas}></canvas>
 
 <style>
     canvas {
---
+++ src/lib/canvasHelpers.js
@@ -0,0 +1,17 @@
+
+    //Inialize canvas
+export function initCanvas(canvas) {
+    ctx = canvas.getContext('2d');
+    if (!ctx) {
+        throw new Error('Failed to get 2D context for canvas');
+    }
+    console.log('canvas initalized');
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    return ctx;
+}
+
+export function resizeCanvas(canvas) {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+}
